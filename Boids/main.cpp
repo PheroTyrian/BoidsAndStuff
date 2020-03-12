294ccf005a643591ed1d76cc656478bfa04d7cb7
PheroTyrian <trislancaster@gmail.com>
2020-03-12 01:46:36 +0000

    Start of opengl implementation

--- Boids/main.cpp
+++ Boids/main.cpp
@@ -53,6 +53,19 @@ int main()
 		obstacles.emplace_back((rand() % 201) - 100, (rand() % 201) - 100, (rand() % 201) - 100);
 	}
 
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+	};
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
 	//Loop updates until the window is closed
 	clock_t timeCounter = clock();
 	while (!glfwWindowShouldClose(window))
@@ -70,8 +83,9 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.6f, 0.3f, 1.0f);
+		//glClearColor(0.2f, 0.6f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
