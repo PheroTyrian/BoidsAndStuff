660758d8a4630a328e5e5989b13308ea5d8e9eb7
PheroTyrian <trislancaster@gmail.com>
2020-03-23 22:15:22 +0000

    Shader first pass and test

--- Boids/main.cpp
+++ Boids/main.cpp
@@ -10,6 +10,47 @@
 constexpr int numBoids = 100;
 constexpr int numObst = 40;
 
+static int compileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length*sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Shader compile error:" << std::endl << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	//Delete shader intermediaries
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main()
 {
 	//Basic setup
@@ -66,6 +107,23 @@ int main()
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	std::string vertexShader =
+		"#version 330 core \n"
+		"layout(location = 0) in vec4 position;\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = position;\n"
+		"}\n";
+	std::string fragmentShader =
+		"#version 330 core \n"
+		"layout(location = 0) out vec4 colour;\n"
+		"void main()\n"
+		"{\n"
+		"colour = vec4(1.0, 1.0, 0.0, 1.0);\n"
+		"}\n";
+	unsigned int shader = createShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	//Loop updates until the window is closed
 	clock_t timeCounter = clock();
